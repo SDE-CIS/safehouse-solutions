52dd0f8ca84d1825bee2846d13cbd9fcb28c7e83
Sebastian Andersen <esocietyhere@gmail.com>
2025-10-21 13:31:26 +0200

    update contact us page

--- src/app/routes/contact-us.tsx
+++ src/app/routes/contact-us.tsx
@@ -1,21 +1,33 @@
 import { Container, Heading, Stack, Text } from '@chakra-ui/react';
 import { Newsletter } from "@/components/ui/newsletter.tsx";
 import { useTranslation } from "react-i18next";
+import { Banner } from '@/components/ui/banner';
 
 export function ContactUsRoute(): JSX.Element {
     const { t } = useTranslation();
 
     return (
-        <Container maxW="container.xl" flex="1" py={10}>
-            <Stack gap={8} align="center">
-                <Heading as="h1" size="xl" textAlign="center">
-                    {t('contact_us.title')}
-                </Heading>
-                <Text fontSize="lg" textAlign="center" color="gray.600">
-                    {t('contact_us.description')}
-                </Text>
-                <Newsletter />
-            </Stack>
-        </Container>
+        <>
+            <Banner
+                imageUrl="/images/privacy-policy.jpg"
+                title={t('contact_us.title')}
+                description={t('contact_us.description')}
+                size='sm'
+                overlayColor="rgba(0, 0, 0, 0.3)"
+            />
+
+            <Container py={10}>
+                <Stack gap={8} align="center">
+                    <Heading as="h1" size="xl" textAlign="center">
+                        {t('contact_us.intro_title')}
+                    </Heading>
+                    <Text fontSize="lg" textAlign="center" color="gray.600">
+                        {t('contact_us.intro_description')}
+                    </Text>
+                    <Newsletter />
+                </Stack>
+
+            </Container>
+        </>
     );
 }
