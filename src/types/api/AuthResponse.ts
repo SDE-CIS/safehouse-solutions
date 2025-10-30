77f00a9d239853a4cd2862cdd4b7a9449ec789ea
Sebastian Andersen <esocietyhere@gmail.com>
2025-10-30 10:08:29 +0100

    update auth response

--- src/types/api/AuthResponse.ts
+++ src/types/api/AuthResponse.ts
@@ -1,9 +1,20 @@
-export interface AuthResponse {
-    message: string;
-    accessToken: string;
-    refreshToken: string;
-    user: {
-        id: number;
-        username: string;
-    }
-}
+import { z } from "zod";
+
+export const AuthResponseSchema = z.object({
+    success: z.boolean(),
+    accessToken: z.string().optional(),
+    refreshToken: z.string().optional(),
+    user: z.object({
+        id: z.string(),
+        username: z.string(),
+        firstname: z.string().optional(),
+        lastname: z.string().optional(),
+        phoneNumber: z.string().optional(),
+        email: z.string().optional(),
+        pfp: z.object({
+            type: z.string(),
+            data: z.array(z.number()),
+        }).optional(),
+    })
+});
+export type AuthResponse = z.infer<typeof AuthResponseSchema>;
